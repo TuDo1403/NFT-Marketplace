199f815d3fefc7935839e7323732a0723fb1050d
Dat Nguyen <datndt@inspirelab.io>
2022-07-05 09:31:44 +0700

    add comment & modify code convention

--- contracts/v2/Collectible.sol
+++ contracts/v2/Collectible.sol
@@ -2,17 +2,21 @@
 pragma solidity ^0.8.15;
 
 import "./interfaces/ICollectible.sol";
-// import "node_modules/@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
-contract Collectible is 
-    ICollectible
-{
+/// @title Abstract contract for NFT
+/// @author Dat Nguyen (datndt@inspirelab.io)
+
+contract Collectible is ICollectible {
     // State variables
     address private factory;
 
-    // Events
-
-    // Functions
+    /**
+     * @notice Initialize for Collectible contract
+     * @param _admin NFT Contract owner
+     * @param _uri URI of NFT
+     * @param _name Name of NFT
+     * @param _symbol Symboy of NFT
+     */
     function initialize(
         address _admin,
         string calldata _uri,
@@ -20,9 +24,22 @@ contract Collectible is
         string calldata _symbol
     ) external virtual {}
 
+    /**
+     * @notice Get type of Collectible
+     * @return Type of NFT (uint96)
+     */
     function getType() external virtual returns (uint96) {}
 
-    function setTokenURI(uint256 token, bytes memory uri) external virtual {}
+    /**
+     * @notice Set the token URI
+     * @param tokenId Token ID need to set URI
+     * @param uri New URI token
+     */
+    function setTokenURI(uint256 tokenId, bytes memory uri) external virtual {}
 
+    /**
+     * @notice Freeze token URI
+     * @param tokenId Token ID need to freeze URI
+     */
     function freezeTokenURI(uint256 tokenId) external virtual {}
 }
