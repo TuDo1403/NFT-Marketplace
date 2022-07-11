9b16bb2246f5a37192f864f56b0138d2d7fec6fe
Tu Do <tudm@inspirelab.io>
2022-07-11 16:48:40 +0700

    Merge branch 'tudo_dev' into 'main'
    Tudo dev
    
    See merge request triton2/blockchain/triton-smart-contract!1

--- remix-compiler.config.js
+++ remix-compiler.config.js
@@ -2,8 +2,8 @@ module.exports = {
               solidity: '0.8.13',
               settings: {
                 optimizer: {
-                  enabled: false,
-                  runs: 200
+                  enabled: true,
+                  runs: 50
                 }
               }
             }
