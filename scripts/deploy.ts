552b5fd0dee9867373e9b361054c0b3964bc0246
HoangLC <hoanglc@inspirelab.io>
2022-07-21 22:11:33 +0700

    add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,8 +3,18 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import {ethers} from "hardhat"
-import {TokenIdGenerator} from "../typechain"
+import { ethers, upgrades } from "hardhat"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import * as dotenv from "dotenv"
+dotenv.config()
+import { TokenIdGenerator, Governance, Marketplace, NFTFactory, Collectible721, Collectible1155 } from "../typechain"
+
+
+const TREASURY = process.env.TREASURY || "";
+const VERIFIER = process.env.VERIFIER || "";
+const SERVICE_FEE = 128;
+const CREATOR_FEE_UB = 1000;
+
 
 async function main() {
     // Hardhat always runs the compile task when running scripts with its command
@@ -15,14 +25,38 @@ async function main() {
     // await hre.run('compile');
 
     // We get the contract to deploy
-    const TokenIdGeneratorFactory = await ethers.getContractFactory(
-        "TokenIdGenerator"
+    console.log("Deploying Governance contract...");
+    const GovernanceFactory = await ethers.getContractFactory("Governance")
+    const governance = await GovernanceFactory.deploy(
+        TREASURY,
+        VERIFIER,
     )
-    const tokenIdGenerator = await TokenIdGeneratorFactory.deploy()
+    await governance.deployed()
+    console.log("Governance deployed to:", governance.address)
 
-    await tokenIdGenerator.deployed()
+    console.log("Deploying Collectible1155Base contract...");
+    const collectible1155Factory = await ethers.getContractFactory("Collectible1155")
+    const collectible1155Base = await collectible1155Factory.deploy()
+    await collectible1155Base.deployed()
+    console.log("Collectible1155Base deployed to:", collectible1155Base.address)
 
-    console.log("Contract deployed to:", tokenIdGenerator.address)
+    console.log("Deploying Collectible721Base contract...");
+    const Collectible721Factory = await ethers.getContractFactory("Collectible721")
+    const collectible721Base = await Collectible721Factory.deploy()
+    await collectible721Base.deployed()
+    console.log("Collectible721Base deployed to:", collectible721Base.address)
+
+    console.log("Deploying Marketplace contract...");
+    const MarketplaceFactory = await ethers.getContractFactory("Marketplace")
+    const marketplace = (await upgrades.deployProxy(MarketplaceFactory, [governance.address, SERVICE_FEE, CREATOR_FEE_UB], { initializer: "initialize" })) as Marketplace
+    await marketplace.deployed()
+    console.log("Marketplace deployed to: ", marketplace.address)
+
+    console.log("Deploying NFTFactory contract...");
+    const NFTFactoryFactory = await ethers.getContractFactory("NFTFactory")
+    const nftFactory = (await upgrades.deployProxy(NFTFactoryFactory, [governance.address], { initializer: "initialize" })) as NFTFactory
+    await nftFactory.deployed()
+    console.log("NFTFactory deployed to:", nftFactory.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
